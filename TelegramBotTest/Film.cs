93841047eeb2fa8ae002887fecbcdb14bd123a2b
Артем Cкорописов <sckoropisow.ar@yandex.ru>
2023-11-11 19:10:59 +0300

    Реалтзовал основные комманды

---
+++ TelegramBotTest/Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotTest
+{
+    public class Film
+    {
+        public string Name { get; set; }
+        public string Owner {get; set;}
+        public string Discription { get; set; }
+        public string URL { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
--- TelegramBotTest/Program.cs
+++ TelegramBotTest/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBotTest
 {
     class Program
     {
         static readonly ITelegramBotClient bot = new TelegramBotClient("6746153930:AAFwNdo8EFHLpGYvS9u5PvoqGNV-Kyxa3NU");
+
+        public static ObservableCollection<Film> Films { get; set; } = new ObservableCollection<Film>();
 
 
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
@@ -20,6 +24,11 @@ namespace TelegramBotTest
             {
                 var message = update.Message;
                 string command = message.Text.ToLower();
+                int probel = command.IndexOf(" ");
+                if (probel > 0)
+                {
+                    command = command.Substring(0, probel);
+                }
 
                 switch (command)
                 {
@@ -28,15 +37,25 @@ namespace TelegramBotTest
                         await Command(botClient, message);
                         break;
                     case "/getlist":
+                        await GetListFilms(botClient, message);
                         break;
-                    case "/additem":
+                    case "+":
+                        await AddItem(botClient, message, probel);
                         break;
-                    case "/removeitem":
+                    case "-":
+                        await RemoveItem(botClient, message, probel);
                         break;
-                    case "/checkitem":
+                    case "check":
+                        await Cheack(botClient, message, probel);
+                        break;
+                    case "/clear":
+                        await ClearAll(botClient, message);
                         break;
                     case "/command":
                         await Command(botClient, message);
+                        break;
+                    case "/secret":
+                        await botClient.SendTextMessageAsync(message.Chat, "Ты у меня самая лучшая\n");
                         break;
                     default:
                     {
@@ -52,10 +71,65 @@ namespace TelegramBotTest
             await botClient.SendTextMessageAsync(message.Chat,
                             "Список доступных команд: \n" +
                             "/GetList - Получить список фильмов, \n" +
-                            "/AddItem - Добавить фильм, \n" +
-                            "/RemoveItem - Удалить фильм, \n" +
-                            "/CheckItem - Отметить фильм как просмотренный \n" +
+                            "+ \"название фильма\" - Добавить фильм, \n" +
+                            "- \"название фильма\" - Удалить фильм, \n" +
+                            "Check \"название фильма\" - Отметить фильм как просмотренный \n" +
+                            "/Clear - Удалить все \n" +
+                            "Secret - <3 \n" +
                             "/Command - Получить список доступных команд \n");
+            return;
+        }
+
+        private static async Task AddItem(ITelegramBotClient botClient, Message message, int probel)
+        {
+            Film newFilm = new Film();
+            newFilm.Name = message.Text.Substring(probel + 1);
+            newFilm.Owner = message.From.FirstName;
+            Films.Add(newFilm);
+            return;
+        }
+
+        private static async Task RemoveItem(ITelegramBotClient botClient, Message message, int probel)
+        {
+            Film newFilm = new Film();
+            newFilm.Name = message.Text.Substring(probel + 1);
+            Films.Remove(newFilm);
+            return;
+        }
+
+        private static async Task Cheack(ITelegramBotClient botClient, Message message, int probel)
+        {
+            string name = message.Text.Substring(probel + 1);
+
+            foreach (var f in Films)
+            {
+                if (f.Name == name) f.IsChecked = true;
+            }
+            return;
+        }
+
+        private static async Task GetListFilms(ITelegramBotClient botClient, Message message)
+        {
+            string films = string.Empty;
+            if (Films.Count < 1) await botClient.SendTextMessageAsync(message.Chat, "Список фильмов пока пуст =(");
+            else
+            {
+                foreach (var f in Films)
+                {
+                    string isChecked = string.Empty;
+                    if (f.IsChecked) isChecked = "✓"; else isChecked = "✕";
+
+                    films += (f.Name +" " + (isChecked + " " + f.Owner) + "\n");
+                }
+                await botClient.SendTextMessageAsync(message.Chat, films);
+            }
+            return;
+        }
+
+        private static async Task ClearAll(ITelegramBotClient botClient, Message message)
+        {
+            Films.Clear();
+            await botClient.SendTextMessageAsync(message.Chat, "Список фильмов очищен, скорее добавь новые!");
             return;
         }
 
